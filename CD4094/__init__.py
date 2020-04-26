8beba98270eab591db33f9fcd739856c326f4802
phillipdavidstearns <phil@phillipstearns.com>
2020-04-26 15:59:04 -0400

    fixing broken package

---
+++ CD4094/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+from .CD4094 import enable
+from .CD4094 import disable
+from .CD4094 import clear
+from .CD4094 import start
+from .CD4094 import stop
+from .CD4094 import update
+from .CD4094 import init
--- __init__.py
+++ __init__.py
@@ -1,8 +1,3 @@
 # __init__.py
-from .CD4094 import enable
-from .CD4094 import disable
-from .CD4094 import clear
-from .CD4094 import start
-from .CD4094 import stop
-from .CD4094 import update
-from .CD4094 import init
+import RPi.GPIO
+import CD4094
