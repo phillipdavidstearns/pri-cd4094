99f57d9ae0e19fdd941c134c06f269a833a67bb3
phillipdavidstearns <phil@phillipstearns.com>
2020-04-30 08:42:23 -0400

    removed start() and made argument checks in init() more potent

--- CD4094/CD4094.py
+++ CD4094/CD4094.py
@@ -24,15 +24,10 @@ def disable():
 def clear():
 	GPIO.output(DATA, 0)
 	for c in range(CHANNELS):
-		GPIO.output(CLOCK, 0)
 		GPIO.output(CLOCK, 1)
-	GPIO.output(CLOCK, 0)
+		GPIO.output(CLOCK, 0)
 	GPIO.output(STROBE, 1)
 	GPIO.output(STROBE, 0)
-
-def start():
-	clear()
-	enable()
 
 def stop():
 	disable()
@@ -42,13 +37,11 @@ def stop():
 # takes a list of boolean values and outputs them
 def update(values):
 	for c in range(CHANNELS):
-		GPIO.output(CLOCK, 0)
 		GPIO.output(DATA, values[CHANNELS - c - 1])
 		GPIO.output(CLOCK, 1)
-	GPIO.output(CLOCK, 0)
+		GPIO.output(CLOCK, 0)
 	GPIO.output(STROBE, 1)
 	GPIO.output(STROBE, 0)
-	GPIO.output(DATA, 0)
 
 def init(pins, channels):
 
@@ -61,23 +54,29 @@ def init(pins, channels):
 	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM) # use GPIO pin numbers
 
-	# Setup Rregister outputs
-
-	STROBE=pins[0]
-	DATA=pins[1]
-	CLOCK=pins[2]
-	ENABLE=pins[3]
-	CHANNELS=channels
-
-	if STROBE == -1 or DATA == -1 or CLOCK == -1 or ENABLE == -1:
+	# Setup register outputs
+	
+	if len(pins) == 4:
+		STROBE, DATA, CLOCK, ENABLE = pins
+	else:
 		print("Registers require 4 GPIO pins: strobe, data, clock, and enable")
 		return
 
-	if CHANNELS == -1:
+	if STROBE < 1 or STROBE > 40 or DATA < 1 or DATA > 40 or CLOCK < 1 or CLOCK > 40 or ENABLE < 1 or ENABLE > 40:
+		print("GPIO pins must be positive integers from 1-40")
+		return
+
+	CHANNELS = channels
+
+	if CHANNELS is None:
+		print("Number of channels must be an integer")
+		return
+	elif CHANNELS <= 0:
 		print("Number of channels must be greater than 0")
 		return
 
 	for pin in pins: 
 		GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
 
-	start()
+	clear()
+	enable()
